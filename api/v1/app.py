c2245df5c121e51288072ffde23d2122a1bde478
RM92023 <robinjara20@gmail.com>
2023-08-24 15:00:51 -0500

    update folder api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
-
-"""
-This module contains the principal application
-app to connect to api
-"""
-
-import os
+"""Script that starts a Flask app"""
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
-from flask_cors import CORS
+from os import getenv
 
+
+"""Start Flask"""
 app = Flask(__name__)
+
+"""Register the blueprint app_views"""
 app.register_blueprint(app_views)
-cors = CORS(app, origins="0.0.0.0")
+
+"""Create the CORS instance to allow IPs"""
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
-    """ calls methods close() """
+def teardown(exception):
+    """Closes session"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """ Loads a custom 404 page not found """
-    return jsonify(error="Not found"), 404
+def errorhandler(error):
+    """Returns a JSON-formated status code for errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.getenv(
-            'HBNB_API_HOST', '0.0.0.0'), port=int(
-            os.getenv(
-                'HBNB_API_PORT', '5000')), threaded=True)
+    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    API_PORT = getenv("HBNB_API_PORT", 5000)
+    app.run(host=API_HOST, port=API_PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""index file"""
-
+"""Flask application that retrieves information"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
-from models import storage
-DicText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """status"""
+    """Returns the app status"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def stats():
-    """stats"""
-    return_dict = {}
-    for key, value in DicText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-
-if __name__ == "__main__":
-    pass
+    """Retrieves the number of objects per each type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+    import json
+    dic = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    json_dict = json.dumps(dic, indent=2)
+    return json_dict
